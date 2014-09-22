2b60d19d7cadf4ef97881d18402ab47a08a579a2
ev <ev@evang.dk>
2014-09-22 20:17:43 +0200

    adds project to git

---
+++ ShapeOOP/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Shapes;
+
+namespace ShapeOOP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //my variables
+        List<MyEllipse> ellipses = new List<MyEllipse>(); 
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // create MyEllipse object
+            MyEllipse myEllipse = new MyEllipse(200, 150, 500, 300, Colors.RoyalBlue);
+            ellipses.Add(myEllipse);
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = myEllipse.width;
+            ellipse.Height = myEllipse.height;
+            ellipse.Margin = new Thickness(myEllipse.positionX, myEllipse.positionY,0,0);
+            ellipse.Fill = new SolidColorBrush(myEllipse.color);
+     
+            MyGrid.Children.Add(ellipse);
+        }
+
+        
+    }
+}
---
+++ ShapeOOP/MyEllipse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace ShapeOOP
+{
+    class MyEllipse
+    {
+        public int width;
+        public int height;
+        public int positionX;
+        public int positionY;
+        public Color color;
+
+        //constructor - used when objects are created
+        public MyEllipse(int width, int height, int positionX, int positionY, Color color)
+        {
+            this.width = width;
+            this.height = height;
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.color = color;
+        }
+
+
+    }
+}
